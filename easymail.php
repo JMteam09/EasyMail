63221887c704c3a3bd6ddcb1f8b15e496c1bd077
JMteam09 <jmgamerzzz@gmail.com>
2016-01-31 03:53:42 +0100

    EM.php
    Error fix

--- easymail.php
+++ easymail.php
@@ -6,6 +6,6 @@ $json["sub"] = sub;
 $json["txt"] = $txt;
 $json["html"] = $html;
 file_put_contents("C:\easymail.txt",json_encode($json));
-return exec('"C:\Users\Jordi\Desktop\VisualBasicProjects\ConsoleApplication2\ConsoleApplication2\bin\Debug\ConsoleApplication2.exe" -fb -path:C:\easymail.txt"');
+return exec('"EMConsoleApplication2.exe" -fb -path:C:\\easymail.txt"');
 }
 ?>
