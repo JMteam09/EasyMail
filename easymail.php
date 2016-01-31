e2711dd198996dd3100180b2a6b06132984527c6
JMteam09 <jmgamerzzz@gmail.com>
2016-01-31 03:36:18 +0100

    Core files added
    Easy use

---
+++ easymail.php
@@ -0,0 +1,11 @@
+﻿<?php
+function email($to,$sub,$txt,$html){
+$json = array();
+$json["who"] = $to
+$json["sub"] = sub
+$json["txt"] = $txt
+$json["html"] = $html
+file_put_contents("C:\easymail.txt",json_encode($json));
+return exec('"C:\Users\Jordi\Desktop\VisualBasicProjects\ConsoleApplication2\ConsoleApplication2\bin\Debug\ConsoleApplication2.exe" -fb -path:C:\easymail.txt"');
+}
+?>
