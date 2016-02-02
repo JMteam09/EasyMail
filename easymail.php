b746da4c3b9c62c4a8b6691c03bec7fb1bb6bf4d
JMteam09 <jmgamerzzz@gmail.com>
2016-02-03 00:04:58 +0100

    PHP api updated

--- easymail.php
+++ easymail.php
@@ -1,11 +1,16 @@
 ﻿<?php
-function email($to,$sub,$txt,$html){
+function email($to,$sub,$txt,$html, $async = true){
 $json = array();
 $json["who"] = $to;
 $json["sub"] = $sub;
 $json["txt"] = $txt;
 $json["html"] = $html;
 file_put_contents("C:\\easymail.txt",json_encode($json));
-return exec('"'.realpath(dirname(__FILE__)).'\\EasyMail.exe" -fb -path:C:\\easymail.txt');
+if ($async){
+	return exec('"'.realpath(dirname(__FILE__)).'\\EasyMail.exe" -fb -path:C:\\easymail.txt');
+} else {
+	exec ('"'.realpath(dirname(__FILE__)).'\\EasyMail.exe" -fb -path:C:\\easymail.txt' . "> /dev/null 2>/dev/null &")
+	return true;
+}
 }
 ?>
