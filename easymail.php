398883691efbabcbf178bb0764884e689f16a043
JMteam09 <jmgamerzzz@gmail.com>
2016-03-19 17:07:42 +0100

    Updated script.bat

--- easymail.php
+++ easymail.php
@@ -6,7 +6,9 @@ $json["sub"] = $sub;
 $json["txt"] = $txt;
 $json["html"] = $html;
 file_put_contents("C:\\easymail.txt",json_encode($json));
-exec('start "'.realpath(dirname(__FILE__)).'\\EasyMail.exe" -fb -path:C:\\easymail.txt');
+$em_script = 'start "EM" "'.realpath(dirname(__FILE__)).'\\EasyMail.exe" -fb -path:C:\\easymail.txt';
+file_put_contents(dirname(__FILE__)+"/script.bat",json_encode($json));
+exec(dirname(__FILE__)+"/script.bat");
 return "operation started";
 }
 ?>
