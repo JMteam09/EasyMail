416dc184f75e0a01d47d2134538077c24780b4d1
JMteam09 <jmgamerzzz@gmail.com>
2016-03-19 16:43:10 +0100

    Added async
    Removed return statement of exec

--- easymail.php
+++ easymail.php
@@ -6,6 +6,7 @@ $json["sub"] = $sub;
 $json["txt"] = $txt;
 $json["html"] = $html;
 file_put_contents("C:\\easymail.txt",json_encode($json));
-return exec('"'.realpath(dirname(__FILE__)).'\\EasyMail.exe" -fb -path:C:\\easymail.txt');
+exec('start "'.realpath(dirname(__FILE__)).'\\EasyMail.exe" -fb -path:C:\\easymail.txt');
+return "operation started"
 }
 ?>
